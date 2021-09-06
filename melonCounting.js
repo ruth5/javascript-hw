f62183e1cdb3ab1994b3c64ba165752d00654e09
Ruth Rosenthal <ruthbrosenthal@gmail.com>
2021-09-06 04:36:51 +0000

    finished assignment

---
+++ melonCounting.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const melonsToAdd = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba'];
+
+const countMelons = (melonArray) => {
+    const melonCounts = {};
+    for (const melon of melonArray) {
+        if (melonCounts[melon]) {
+            melonCounts[melon] = melonCounts[melon] + 1;
+        }
+        else {
+            melonCounts[melon] = 1;
+        }
+
+    }
+    return melonCounts;
+}
+
+console.log(countMelons(melonsToAdd));
+
